90dff3b43cd487bfb33c1461a687f70ee063e0d9
gremble0 <hermanstornes@gmail.com>
2024-09-16 14:54:04 +0200

    AAAAAAAAAAAAAAAAAAAAAAAAAAAAA

--- src/main/java/com/booleanuk/library/config/SecurityConfiguration.java
+++ src/main/java/com/booleanuk/library/config/SecurityConfiguration.java
@@ -1,6 +1,5 @@
 package com.booleanuk.library.config;
 
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -8,7 +7,6 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.annotation.web.configurers.LogoutConfigurer;
 import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
@@ -18,8 +16,12 @@ import org.springframework.security.web.SecurityFilterChain;
 public class SecurityConfiguration {
   @Bean
   public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-    return http.csrf(AbstractHttpConfigurer::disable)
-        .authorizeHttpRequests(requests -> requests.requestMatchers("/games", "/games/*", "/developers", "/developers/*", "/users", "/users/*").authenticated())
+    return http
+        .csrf(AbstractHttpConfigurer::disable)
+        .authorizeHttpRequests(requests -> requests
+            .requestMatchers("/games", "/games/*", "/developers", "/developers/*", "/users", "/users/*").authenticated()
+            .requestMatchers("/login").permitAll()
+        )
         .formLogin(form -> form.loginPage("/login").permitAll())
         .logout(LogoutConfigurer::permitAll)
         .build();
@@ -27,12 +29,9 @@ public class SecurityConfiguration {
 
   @Bean
   public UserDetailsService userDetailsService() {
-    UserDetails user = User.withDefaultPasswordEncoder()
-        .username("user")
+    return new InMemoryUserDetailsManager(User.withUsername("user")
         .password("password")
-        .roles("USER")
-        .build();
-
-    return new InMemoryUserDetailsManager(user);
+        .roles("ADMIN")
+        .build());
   }
 }
